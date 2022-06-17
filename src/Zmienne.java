689cf009e5ea5cbbe9af5f80dbea9c1266e109c7
przemjan <przemjanisz2@gmail.com>
2022-06-17 10:28:54 +0200

    commit 5

---
+++ src/Zmienne.java
@@ -0,0 +1,22 @@
+public class Zmienne {
+    public static void main(String[] args) {
+
+        int liczba0;
+        liczba0 = 10;
+
+        double liczba1 = 123.45;
+
+//        System.out.println(liczba0);
+//        System.out.println(liczba1);
+
+        String napis = "Jakiś\n \"tekst\"";
+        System.out.print("Liczba\t wynosi\n "+ liczba0 +" druga liczba: " + liczba1+"\n");
+        System.out.println(napis);
+
+        char znak= 't';
+
+        boolean tw = true; //false
+
+
+    }
+}
