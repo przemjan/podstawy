f251a4a94057ad4dfa3674a022b86433efbfc6d2
przemjan <przemjanisz2@gmail.com>
2022-06-17 12:16:41 +0200

    commit 7

---
+++ src/Dzialania.java
@@ -0,0 +1,29 @@
+public class Dzialania {
+    public static void main(String[] args) {
+
+        // x przechowuje godziny
+
+        int godziny = 4;
+        double srednia =5.9;
+
+
+        int temp;
+        temp = (int)srednia;
+
+        System.out.println(temp);
+
+        double wynik = godziny + srednia;
+
+
+        int WORK_HOURS = 8;
+        final int WORK_DAYS = 5;
+        final int WEEKS = 52;
+
+
+
+        wynik = WORK_HOURS * WORK_DAYS * WEEKS;
+
+
+
+    }
+}
